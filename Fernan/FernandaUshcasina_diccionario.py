63133af3f16d7d1229200f2f5c25be75ff52332f
USUARIO10-PC\Usuario01 <chikifey2122@hotmail.com>
2016-11-16 13:43:38 -0500

    Diccionario de telefono

---
+++ Fernan/FernandaUshcasina_diccionario.py
@@ -0,0 +1,14 @@
+##Diccionario
+tel={ 'Gaby': 3121657, 'Kathy': 3225958, 'Juank': 2322646}
+## agrega un telefono
+tel ['Adry']= 2559988
+print("\nTelefono")
+print(tel)
+
+## agregando otro telefono
+print("\nAgregando otro Telefono")
+tel ['Adela'] = 3226564
+
+## lista de pesonas en el telefono
+print (tel)
+
